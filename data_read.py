d1293941c5fa9255a290215c0188c03253d19de6
FranciscoSotoU <soto.u.f@gmail.com>
2023-04-12 20:26:16 -0400

    run

--- data_read.py
+++ data_read.py
@@ -2,17 +2,10 @@ from light_curve import LightCurve
 import joblib
 from torch.utils.data import Dataset, DataLoader
 
-path_macho = '/Users/francisco/Documents/data/macho/full.pkl'
-data_macho = joblib.load(path_macho)
-
-path_asas = '/Users/francisco/Documents/data/asas/full.pkl'
-data_asas = joblib.load(path_asas)
-
-path_linear = '/Users/francisco/Documents/data/linear/full.pkl'
-data_linear = joblib.load(path_linear)
-
-all_data = data_macho + data_asas + data_linear
-
-class data(Dataset):
+class LCD(Dataset):
     def __init__(self,data):
-        super().__init__()
+        self.data = data
+    def __len__(self):
+        return len(self.data)
+    def __get_item__(self,index):
+        return self.data[index]
